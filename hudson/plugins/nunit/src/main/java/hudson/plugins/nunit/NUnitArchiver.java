26f841b8afa97ee4b8f8de7969d90418235b89b7
null <null@dev.java.net>
2007-11-30 21:48:27 +0000

    This commit was manufactured by cvs2svn to create tag 'nunit-0_4'.

---
+++ hudson/plugins/nunit/src/main/java/hudson/plugins/nunit/NUnitArchiver.java
@@ -0,0 +1,143 @@
+package hudson.plugins.nunit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.FileSet;
+import org.xml.sax.SAXException;
+
+import hudson.AbortException;
+import hudson.FilePath;
+import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
+import hudson.util.IOException2;
+
+/**
+ * Class responsible for transforming NUnit to JUnit files and then run them all through the JUnit result archiver.
+ * 
+ * @author Erik Ramfelt
+ */
+public class NUnitArchiver implements FilePath.FileCallable<Boolean> {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String JUNIT_REPORTS_PATH = "temporary-junit-reports";
+
+    private transient final String testResultsPattern;
+    private transient boolean keepJUnitReports = false;
+    private transient boolean skipJUnitArchiver = false;
+
+    // Build related objects
+    private transient final BuildListener listener;
+
+    private transient TestReportTransformer unitReportTransformer;
+    private transient TestReportArchiver unitResultArchiver;;
+
+    public NUnitArchiver(BuildListener listener, String testResults,
+            TestReportArchiver unitResultArchiver, TestReportTransformer transformer, boolean keepJUnitReports,
+            boolean skipJUnitArchiver) throws TransformerException, ParserConfigurationException {
+
+        this.unitResultArchiver = unitResultArchiver;
+        this.listener = listener;
+        this.testResultsPattern = testResults;
+        this.unitReportTransformer = transformer;
+        this.keepJUnitReports = keepJUnitReports;
+        this.skipJUnitArchiver = skipJUnitArchiver;
+    }
+
+    /** {@inheritDoc} */
+    public Boolean invoke(File ws, VirtualChannel channel) throws IOException {
+        Boolean retValue = Boolean.FALSE;
+        listener.getLogger().println("Transforming NUnit tests results");
+        String[] nunitFiles = findNUnitReports(ws);
+        if (nunitFiles.length > 0) {
+            File junitOutputPath = new File(ws, JUNIT_REPORTS_PATH);
+            junitOutputPath.mkdirs();
+    
+            for (String nunitFileName : nunitFiles) {
+                FileInputStream fileStream = new FileInputStream(new File(ws, nunitFileName));
+                try {
+                    // Transform all NUnit files
+                    // listener.getLogger().println("Transforming " +
+                    // nunitFileName);
+                    unitReportTransformer.transform(fileStream, junitOutputPath);
+                } catch (TransformerException te) {
+                    throw new IOException2(
+                            "Could not transform the NUnit report. Please report this issue to the plugin author", te);
+                } catch (SAXException se) {
+                    throw new IOException2(
+                            "Could not transform the NUnit report. Please report this issue to the plugin author", se);
+                } finally {
+                    fileStream.close();
+                }
+            }
+    
+            if (skipJUnitArchiver) {
+                listener.getLogger().println("Skipping feeding JUnit reports to JUnitArchiver");
+            } else {
+                // Run the JUnit test archiver
+                retValue = performJUnitArchiver();
+            }
+    
+            if (keepJUnitReports) {
+                listener.getLogger().println("Skipping deletion of temporary JUnit reports.");
+            } else {
+                // Delete JUnit report files and temp folder
+                // listener.getLogger().println("Deleting transformed JUnit
+                // results");
+                for (File file : junitOutputPath.listFiles()) {
+                    file.delete();
+                }
+                junitOutputPath.delete();
+            }
+        }
+
+        return retValue;
+    }
+
+    /**
+     * Return all NUnit report files
+     * 
+     * @param parentPath parent
+     * @return an array of strings
+     */
+    private String[] findNUnitReports(File parentPath) throws AbortException {
+        FileSet fs = new FileSet();
+        Project p = new Project();
+        fs.setProject(p);
+        fs.setDir(parentPath);
+        fs.setIncludes(testResultsPattern);
+        DirectoryScanner ds = fs.getDirectoryScanner(p);
+
+        String[] nunitFiles = ds.getIncludedFiles();
+        if (nunitFiles.length == 0) {
+            // no test result. Most likely a configuration error or fatal problem
+            listener.fatalError("No NUnit test report files were found. Configuration error?");
+        }
+        return nunitFiles;
+    }
+
+    /**
+     * Run all JUnit reports through the JUnit archiver
+     * 
+     * @return true if the JUnitResultArchiver was successful; false otherwise
+     * @throws IOException thrown if the JUnitResultArchiver.perform() methods throws it
+     */
+    private Boolean performJUnitArchiver() throws IOException {
+        Boolean retValue = Boolean.TRUE;
+        try {
+            if (!unitResultArchiver.archive()) {
+                retValue = Boolean.FALSE;
+            }
+        } catch (InterruptedException ie) {
+            throw new IOException2(ie);
+        }
+        return retValue;
+    }
+}
---
+++ hudson/plugins/nunit/src/main/java/hudson/plugins/nunit/NUnitPublisher.java
@@ -0,0 +1,129 @@
+package hudson.plugins.nunit;
+
+import hudson.Launcher;
+import hudson.maven.agent.AbortException;
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+import hudson.model.BuildListener;
+import hudson.model.Descriptor;
+import hudson.tasks.Publisher;
+import hudson.tasks.junit.JUnitResultArchiver;
+import hudson.tasks.test.TestResultProjectAction;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Class that records NUnit test reports into Hudson.
+ * 
+ * @author Erik Ramfelt
+ */
+public class NUnitPublisher extends hudson.tasks.Publisher implements TestReportArchiver {
+
+    private static transient final String PLUGIN_NUNIT = "/plugin/nunit/";
+
+    public static final Descriptor<Publisher> DESCRIPTOR = new DescriptorImpl();
+
+    private String testResultsPattern;
+    private boolean debug = false;
+    private boolean keepJUnitReports = false;
+    private boolean skipJUnitArchiver = false;
+
+    private AbstractBuild<?, ?> build;
+    private Launcher launcher;
+    private BuildListener listener;
+
+    public NUnitPublisher(String testResultsPattern, boolean debug, boolean keepJUnitReports, boolean skipJUnitArchiver) {
+        this.testResultsPattern = testResultsPattern;
+        this.debug = debug;
+        if (this.debug) {
+            this.keepJUnitReports = keepJUnitReports;
+            this.skipJUnitArchiver = skipJUnitArchiver;
+        }
+    }
+
+    public String getTestResultsPattern() {
+        return testResultsPattern;
+    }
+
+    public boolean getDebug() {
+        return debug;
+    }
+
+    public boolean getKeepJunitReports() {
+        return keepJUnitReports;
+    }
+
+    public boolean getSkipJunitArchiver() {
+        return skipJUnitArchiver;
+    }
+
+    @Override
+    public Action getProjectAction(hudson.model.Project project) {
+        return new TestResultProjectAction(project);
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+            throws InterruptedException, IOException {
+        this.build = build;
+        this.launcher = launcher;
+        this.listener = listener;
+        if (debug) {
+            listener.getLogger().println("NUnit publisher running in debug mode.");
+        }
+        Boolean result = Boolean.FALSE;
+        try {
+            NUnitArchiver transformer = new NUnitArchiver(listener, testResultsPattern, this,
+                    new NUnitReportTransformer(), keepJUnitReports, skipJUnitArchiver);
+            result = build.getProject().getWorkspace().act(transformer);
+        } catch (TransformerException te) {
+            throw new AbortException("Could not read the XSL XML file. Please report this issue to the plugin author",
+                    te);
+        } catch (ParserConfigurationException pce) {
+            throw new AbortException(
+                    "Could not initalize the XML parser. Please report this issue to the plugin author", pce);
+        }
+
+        return result.booleanValue();
+    }
+
+    public boolean archive()
+            throws java.lang.InterruptedException, java.io.IOException {
+        return new JUnitResultArchiver(NUnitArchiver.JUNIT_REPORTS_PATH + "/TEST-*.xml").perform(build, launcher,
+                listener);
+    }
+
+    public Descriptor<Publisher> getDescriptor() {
+        return DESCRIPTOR;
+    }
+
+    public static class DescriptorImpl extends Descriptor<Publisher> {
+
+        protected DescriptorImpl() {
+            super(NUnitPublisher.class);
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Publish NUnit test result report";
+        }
+
+        @Override
+        public String getHelpFile() {
+            return PLUGIN_NUNIT + "help.html";
+        }
+
+        @Override
+        public Publisher newInstance(StaplerRequest req) throws FormException {
+            return new NUnitPublisher(req.getParameter("nunit_reports.pattern"), 
+                    (req.hasParameter("nunit_reports.debug") ? Boolean.parseBoolean(req.getParameter("nunit_reports.debug")) : false), 
+                    (req.hasParameter("nunit_reports.keepjunitreports") ? Boolean.parseBoolean(req.getParameter("nunit_reports.keepjunitreports")) : false), 
+                    (req.hasParameter("nunit_reports.skipjunitarchiver") ? Boolean.parseBoolean(req.getParameter("nunit_reports.skipjunitarchiver")) : false));
+        }
+    }
+}
